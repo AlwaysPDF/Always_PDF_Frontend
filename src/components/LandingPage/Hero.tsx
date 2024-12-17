d1e7f7f4353aeca5e800f343677212c4cfb7769b
BlinksCrib <ajibolaisaac09@gmail.com>
2024-12-17 17:43:24 +0100

    feat: landing page changed 1.2

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -55,21 +55,21 @@ const Hero = () => {
 
     <section className="bg-[#021221] w-full flex justify-center items-center flex-col lmd:mt-14">
       <Navbar />
-      <main className="bg-[#021221] lg:min-h-[60vh] bg-coer w-full ">
-        <div className="bg-[url('/assets/heroshadow.svg')] bg-no-repeat bg-center bg-cover h-full w-full flex justify-start items-center flex-col py-16">
-          {/* <div>
+      <main className="bg-[#021221] lg:min-h-[60vh] bg-coer w-full flex justify-center items-center">
+        <div className="bg-[url('/assets/heroshadow.svg')] bg-no-repeat bg-center bg-cover h-full w-full flex justify-center items-center flex-col py-16">
+          <div>
             <p className="text-white text-sm font-Onest">
               Introducing AlwaysPDF
             </p>
-          </div> */}
-          <h1 className="text-white lg:text-6xl md:text-4xl lmd:text-3xl font-semibold !font-Onest lg:max-w-[40%] text-center">
+          </div>
+          <h1 className="text-white lg:text-7xl md:text-4xl lmd:text-3xl lg:leading-[5rem] font-semibold !font-Onest lg:max-w-[50%] text-center">
             Turn your boring documents into conversations
           </h1>
-          <p className="text-[#E2E8F0] font-Onest text-lg font-medium my-2">
+          {/* <p className="text-[#E2E8F0] font-Onest text-lg font-medium my-2">
             Innovative. Intuitive. Insightful.
-          </p>
-          <span className="text-white text-sm font-light text-center">
-            Welcome to your AI-powered PDF companion.
+          </p> */}
+          <span className="text-white text-sm font-light text-center lg:max-w-[50%]">
+            Ask questions, get instant answers, and uncover insights effortlessly. With AlwaysPDF, your documents finally talk back
           </span>
 
           <button
